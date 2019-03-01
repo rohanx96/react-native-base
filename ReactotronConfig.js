c84c696f875496fd96db4f69ff7364aa25950a37
Rohan Agarwal <rohanx96@gmail.com>
2019-03-02 01:59:44 +0530

    Remove reactotron Update launch options

--- ReactotronConfig.js
+++
@@ -1,8 +0,0 @@
-import Reactotron from "reactotron-react-native";
-import { reactotronRedux } from "reactotron-redux";
-
-const reactotron = Reactotron.configure() // controls connection & communication settings
-  .use(reactotronRedux())
-  .connect(); // let's connect!
-
-export default reactotron;
