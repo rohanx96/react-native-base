9a63bfbc039bdf960bc135948199b1da4c2e1f27
Rohan Agarwal <rohanx96@gmail.com>
2019-03-03 20:10:17 +0530

    Link react native screen and gesture handler for Android

--- android/app/src/main/java/com/reactnativeapp/MainActivity.java
+++ android/app/src/main/java/com/reactnativeapp/MainActivity.java
@@ -2,11 +2,14 @@ package com.reactnativeapp;
 
 import android.os.Bundle;
 
-import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.ReactFragmentActivity;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import org.devio.rn.splashscreen.SplashScreen;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactFragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this);
@@ -20,5 +23,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "ReactNativeApp";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
     }
 }
--- android/app/src/main/java/com/reactnativeapp/MainApplication.java
+++ android/app/src/main/java/com/reactnativeapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.reactnativeapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.swmansion.rnscreens.RNScreensPackage;
 
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new RNGestureHandlerPackage(),
+                    new RNScreensPackage(),
                     new SplashScreenReactPackage()
             );
         }
